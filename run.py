1dd4556bb9b61f540e842d70e198e61e0aaeb40f
Jenny Delaney <jennyelewis29@gmail.com>
2022-10-23 12:29:51 +0000

    Create get_stock_values function to enable print out of calculated stock numbers for each sandwich type.

--- run.py
+++ run.py
@@ -125,7 +125,31 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
-print("Welcome to love sandwiches data automation")
-main()
+print("Welcome to love sandwiches data automation.\n")
+stock_data = main()
+
+
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    # headings = SHEET.worksheet('stock').row_values(1)
+     
+    print(f"Make the following numbers of sandwiches for next market:")
+    
+    # new_data = {}
+    # for heading, stock_num in zip(headings, data):
+    #     new_data[heading] = stock_num
+    # return new_data
+
+    return {headings: data for headings, data in zip(headings, data)}
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
+
+
